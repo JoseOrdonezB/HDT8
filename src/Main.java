793dcc47bd82ba8ea8133b6ed7ae50d532086a77
JS103 <131785570+JoseOrdonezB@users.noreply.github.com>
2024-04-11 21:01:20 -0600

    Código completo sin test

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Process> processQueue = new PriorityQueue<>(Comparator.comparingInt(Process::getNice));
+
+        try (Stream<String> lines = Files.lines(Paths.get("procesos.txt"))) {
+            lines.map(line -> line.split(","))
+                 .filter(processData -> processData.length == 3)
+                 .map(processData -> new Process(processData[0], processData[1], Integer.parseInt(processData[2])))
+                 .forEach(processQueue::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        processQueue.forEach(process -> System.out.println("Process: " + process.getProcessName() +
+                ", User: " + process.getUserName() +
+                ", Priority (PR): " + process));
+    }
+}
